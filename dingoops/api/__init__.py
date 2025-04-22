80acc9223dc81231063737aa74a095e3d30d291a
wangwb <wangwb@zetyun.com>
2025-04-22 18:53:18 +0800

    [feature][resource] Add to resource about api and service partial logic

--- dingoops/api/__init__.py
+++ dingoops/api/__init__.py
@@ -2,7 +2,7 @@
 import os
 
 from fastapi import APIRouter
-from dingoops.api import assets, bigscreens, system, monitor, cluster, node,websocket
+from dingoops.api import assets, bigscreens, system, monitor, cluster, node,websocket, resources
 
 
 # 启动时创建excel的临时存放目录
@@ -15,7 +15,9 @@ api_router.include_router(assets.router, tags=["Assets"])
 api_router.include_router(bigscreens.router, tags=["BigScreens"])
 api_router.include_router(system.router, tags=["Systems"])
 api_router.include_router(monitor.router, tags=["Monitors"])
+
 api_router.include_router(cluster.router, tags=["Cluster"])
 api_router.include_router(node.router, tags=["Node"])
 api_router.include_router(websocket.router, tags=["WebSockets"])
+api_router.include_router(resources.router, tags=["Resources"])
 
