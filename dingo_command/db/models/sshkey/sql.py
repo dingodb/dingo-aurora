99471b8b4eb16efb2d2010e2c14c0707fa569c77
zhangjing2 <zhangjing2@zetyun.com>
2025-09-08 16:53:50 +0800

    [fix][sshkey] debug code for sshkey info

--- dingo_command/db/models/sshkey/sql.py
+++ dingo_command/db/models/sshkey/sql.py
@@ -23,6 +23,8 @@ class KeySQL:
                 query = query.filter(KeyInfo.id == query_params["id"])
             if "name" in query_params and query_params["name"]:
                 query = query.filter(KeyInfo.name == query_params["name"])
+            if "owner" in query_params and query_params["owner"]:
+                query = query.filter(KeyInfo.owner == query_params["owner"])
             if "status" in query_params and query_params["status"]:
                 query = query.filter(KeyInfo.status == query_params["status"])
             if "tenant_id" in query_params and query_params["tenant_id"]:
