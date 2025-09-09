82c41cfcd6e03b602822a1f707d1edbf76702281
zhangjing2 <zhangjing2@zetyun.com>
2025-09-09 16:43:24 +0800

    [fix][sshkey] modify code for sshkey with user_name arg

--- dingo_command/api/model/sshkey.py
+++ dingo_command/api/model/sshkey.py
@@ -13,6 +13,7 @@ ValueType = Union[
 class CreateKeyObject(BaseModel):
     name: Optional[str] = Field(None, description="sshkey的name")
     user_id: Optional[str] = Field(None, description="用户id")
+    user_name: Optional[str] = Field(None, description="用户名称")
     tenant_id: Optional[str] = Field(None, description="租户id")
     key_content: Optional[str] = Field(None, description="sshkey的内容")
     description: Optional[str] = Field(None, description="sshkey的描述")
