3d3eeaed207a860c01478bd8d4d173685f2a78f8
Ketor <d.ketor@gmail.com>
2025-09-26 20:41:44 +0800

    [fix][dks] temporary disable chart sync job.
    Signed-off-by: Ketor <d.ketor@gmail.com>

--- dingo_command/api/chart.py
+++ dingo_command/api/chart.py
@@ -42,8 +42,8 @@ async def init():
             traceback.print_exc()
             raise e
 
-
-asyncio.run(init())
+# TODO: comment here by ketor, need to test if async works well
+# asyncio.run(init())
 
 @router.post("/repo", summary="helm的repo仓库（异步）", description="创建helm的repo仓库（异步）")
 async def create_repo(repo: CreateRepoObject, background_tasks: BackgroundTasks):
